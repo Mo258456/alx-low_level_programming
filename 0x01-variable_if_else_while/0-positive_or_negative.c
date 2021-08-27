64f69bc207e256315435bd05d1b1d18fae010864
MoHack <moti.davis16@gmail.com>
2021-08-27 13:42:54 -0400

    This commit is for the 0-negative or positive project trial three

--- 0x01-variable_if_else_while/0-positive_or_negative.c
+++ 0x01-variable_if_else_while/0-positive_or_negative.c
@@ -1,20 +1,24 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * This is a program that checks the random number the rand() function gives and checkes whether the number is positive, negative or zero
+ * we used an if and else if statement to check the values of the random number and now what the number is this is called conditioning.
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	iif (n < 0)
-        printf("%d is negative\n", n);   
-    else if (n==0)
-        printf("%d is zero\n", n);
-    else if (n > 0)
-        printf("%d is postive\n", n);
-     return (0);
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	return (0);
 }
+
