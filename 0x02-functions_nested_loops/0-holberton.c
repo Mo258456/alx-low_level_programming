0599723001a0e3925501ab2da419ad490242a33c
MoHack <moti.davis16@gmail.com>
2021-08-30 22:06:40 -0400

    This commit is for the 0-holberton.c project trial one

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the phrase "Holeberton" on the screen
+ * Return: 0 if exited properly, non-zero otherwise
+*/
+
+int main(void)
+{
+	puts("Holberton \n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+# This readme is prepared for the 0x0-functons_nested_loops and it contains the following C projects:
