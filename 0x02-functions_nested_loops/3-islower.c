e3a0467fc459cdd68472dcc99b46a80654cdc8f3
MoHack <60172765+Mo258456@users.noreply.github.com>
2021-08-31 14:07:11 -0400

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,7 +5,7 @@
  * @c: the character to check
  * Return: 1 if c is lowercase, 0 otherwise
  */
-int _islower(int l)
+int _islower(int c)
 {
-return (l >= 'a' && l <= 'z');
+return (c >= 'a' && c <= 'z');
 }
