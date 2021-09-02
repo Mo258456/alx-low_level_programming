0a3c39a4bfd2497340bae87c10aff4ba8bdb87d2
MoHack <60172765+Mo258456@users.noreply.github.com>
2021-09-02 20:20:55 +0300

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * mul - A function that multiplies two integers a and b then gives c as product.
+ * mul - A function that multiplies two int 
+ *a * b then gives c as product.
  * Return: the result of the operation
  */
 int mul(int a, int b)
