709618016bd9cb8ae8f0c851abafa38736ee347d
MoHack <60172765+Mo258456@users.noreply.github.com>
2021-09-02 12:45:35 +0300

    0-main.c test one commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * function that tests and prints if an integer is positive or negative.
+ * It should Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
