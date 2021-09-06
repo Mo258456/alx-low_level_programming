bbdb73aa68af842e313b5a662f05f08d8a709d1e
MoHack <60172765+Mo258456@users.noreply.github.com>
2021-09-06 22:19:16 +0300

    try two

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98 - updates the value to 98 
+ * reset_to_98 - updates the value to 98
  * then pointer points to 98
  * @n: pointer to the variable to update
  */
