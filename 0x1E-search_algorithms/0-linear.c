f153e2886a58e5d9c11d66f00eea0edaef1234b9
MoHack <60172765+Mo258456@users.noreply.github.com>
2022-03-30 22:25:06 -0400

    Last try its get difficult to push it using my terminal

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,27 +1,26 @@
 #include "search_algos.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
- * linear_search - searches through an integer array for value @value
- * @array: pointer to the first element of the array to search in
- * @size: the number of elements in the array
- * @value: the value to search for in @array
- *
- * Return: The first index where value is located, -1 otherwise
- */
+* linear_search - searches for a value in an array of integers
+* using the Linear search algorithm
+* @array: pointer to the first element of the array to search in
+* @size: size of array
+* @value: value to search for
+* Return: first index where value is located if successful else -1
+*/
+
 int linear_search(int *array, size_t size, int value)
 {
-	int idx = -1;
-	size_t i;
+	int i = 0;
 
-	for (i = 0; i < size; i++)
+	while (i < (int)size)
 	{
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		if (array[i] == value)
-		{
-			idx = i;
-			break;
-		}
+			return (i);
+		i++;
 	}
-
-	return (idx);
+	return (-1);
 }
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,43 +1,42 @@
 #include "search_algos.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
- * binary_search - using binary search algorithm, searches through an int
- * array for a value
- * @array: pointer to the first element of the array to search in
- * @size: the number of elements in the array
- * @value: the value to search for in @array
- *
- * Return: The first index where value is located, -1 otherwise
- */
+* binary_search - searches for a value in an array of integers
+* using the binary search algorithm
+* @array: pointer to the first element of the array to search in
+* @size: size of array
+* @value: value to search for
+* Return: ndex where value is located if successful else -1
+*/
+
 int binary_search(int *array, size_t size, int value)
 {
-    int idx = -1;
-    size_t i;
+	int left = 0;
+	int right = size - 1;
 
-    printf("Searching in array: ");
+	while (left <= right)
+	{
+		int mid = left + ((right - left) / 2);
+		int i = left;
 
-    for (i = 0; i < size; i++)
-    {
-        printf("%i", array[i]);
-
-        if (i != size - 1)
-            printf(", ");
-    }
-    printf("\n");
-
-    if (value == array[size / 2])
-    {
-        idx = size / 2;
-    }
-    else if (value < array[size / 2])
-    {
-        printf("Going  left: ");
-        idx = binary_search(array, size / 2, value);
-    }
-    else if (value > array[size / 2])
-    {
-        printf("Going right: ");
-        idx = size - size / 2 - 1 + binary_search(array + (int)size / 2, size - size / 2, value);
-    }
-
-    return idx;
+		printf("Searching in array: ");
+		while (i <= right)
+		{
+			if (i == right)
+				printf("%d", array[i]);
+			else
+				printf("%d, ", array[i]);
+			i++;
+		}
+		printf("\n");
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] > value)
+			right = mid - 1;
+		else
+			left = mid + 1;
+	}
+	return (-1);
 }
